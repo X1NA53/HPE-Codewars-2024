704043ba6f0939dacd7fac62f74ff74fc3996ee6
X1NA53 <ccarmichael987@gmail.com>
2025-01-27 09:51:10 -0600

    prob07, definitely inefficient

---
+++ finished/prob07.java
@@ -0,0 +1,35 @@
+package finished;
+import java.util.Scanner;
+
+public class prob07 {
+    public static void main(String[] args) {
+        prob07 p = new prob07();
+        p.in();
+    }
+    
+    public void in(){
+        Scanner file = new Scanner(getClass().getResourceAsStream("input.txt"));
+        file.useDelimiter(""); //Deliminate by each character
+        int count = 0;
+        boolean printed = false;
+        String curr = file.next();
+        while(file.hasNext()){
+            String next = file.next();
+            if(curr.equals(next)){
+                ++count;
+            } else {
+                if(count == 3){
+                    System.out.print(curr);
+                    printed = true;
+                }
+                count = 0;
+            }
+            if(next.equals("."))
+                break;
+            curr = next;
+        }
+        file.close();
+        if(!printed)
+            System.out.println("No Four of a Kind");
+    }
+}
