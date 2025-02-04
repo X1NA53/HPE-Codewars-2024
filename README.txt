2d4114cea18eed2542e9285b8b6b786a2a052f47
X1NA53 <ccarmichael987@gmail.com>
2025-02-04 12:40:26 -0600

    added pdf

--- README.txt
+++ README.txt
@@ -7,4 +7,4 @@ prob 06 -
 prob 07 -
 prob 08 -
 prob 09 -
-prob 10 -
+prob 10 - Finished.. questionable?
--- prob11.java
+++ prob11.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class prob11 {
+    //* 64 bit: long, 32 bit: int */
     
     public static void main(String[] args) {
         prob11 p = new prob11();
