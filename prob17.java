97fcdda62fdc249248d778906088703983e89a51
X1NA53 <ccarmichael987@gmail.com>
2025-02-24 07:38:56 -0600

    22 working

--- prob17.java
+++ prob17.java
@@ -1,4 +1,4 @@
-package finished;
+
 
 import java.util.*;
 
---
+++ unfin/prob23.java
@@ -0,0 +1,16 @@
+package unfin;
+import java.util.Scanner;
+import javafx.util.Pair;
+
+public class prob23 {
+    
+    public static void main(String[] args) {
+        prob23 p = new prob23();
+        p.in();
+    }
+
+    
+    public void input(){
+        Scanner file = new Scanner(getClass().getResourceAsStream("input.txt"));
+    }
+}
