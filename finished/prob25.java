4176ba2e96f247a5bce5afac4367bc11c410a770
Connor McKelvey <connormmckelvey@gmail.com>
2025-02-26 13:41:49 -0600

    worked on prob26.py

---
+++ finished/prob25.java
@@ -0,0 +1,15 @@
+package finished;
+import java.util.Scanner;
+
+public class prob25 {
+    
+    public static void main(String[] args) {
+        prob25 p = new prob25();
+        p.in();
+    }
+
+    public void in(){
+        Scanner file = new Scanner(getClass().getResourceAsStream("input.txt"));
+        //
+    }
+}
---
+++ unfin/prob26.py
@@ -0,0 +1,71 @@
+import re
+
+class ChaseInformation():
+    def __init__(self,speed,aspeed,apos,hlength,exitdistance,flashlight = None,sdoor = None):
+        self.speed = speed
+        self.aspeed = aspeed
+        self.apos = apos
+        self.hlength = hlength
+        self.exitdistance = exitdistance
+        self.flashlight = flashlight
+        self.sdoor = sdoor
+
+        self.ablinded = 0
+        self.ablocked = 0
+        self.time = 0
+        self.pos = 0.0
+        self.escaped = False
+        self.caught = True
+
+    def doInterval(self):
+        self.time += 1 #increment secounds
+        self.pos += self.speed #move us
+        if self.ablinded > 0:
+            self.ablinded -= 1
+        if self.ablocked > 0:
+            self.ablocked -= 0
+        if self.ablinded == 0 and self.ablocked == 0: #as long as we arnt blinded or blocked, move animatronic
+            self.apos += self.aspeed
+        if self.flashlight > self.pos - self.speed and self.flashlight <= self.pos: #check if moved by flashlight this turn
+            if self.sdoor > self.apos and self.sdoor < self.pos: #check if door inbetween us
+                pass
+            else:
+                self.ablinded = 2 #set blinded condition to 2
+        if self.sdoor > self.apos - self.aspeed and self.sdoor <= self.apos: #check if door passed by ani
+            self.ablocked = 3
+        if self.pos <= self.apos:
+            self.caught = True
+        if self.pos >= self.exitdistance:
+            self.escaped = True
+        return [self.str_of_hero_interval(),self.str_of_animatronic_interval()]
+
+
+    def str_of_hero_interval(self):
+        if self.time > 10:
+            return "Time-"+self.time+": "+self.pos-self.speed+"->"+self.pos
+        else:
+            return "Time-"+self.time+": "+self.pos-self.speed+"->"+self.pos
+
+    def str_of_animatronic_interval(self):
+        pass
+
+def getInput():
+    with open("input.txt","r") as file:
+        line = file.readline()
+        speed         = re.findall(r'(?<=Run:)\d+\.\d+',line)[0]
+        aspeed        = re.findall(r'(?<=Chase:)\d+\.\d+',line)[0]
+        apos          = re.findall(r'(?<=Start:-)\d+\.\d+',line)[0]
+        hlength       = re.findall(r'(?<=Length:)\d+\.\d+',line)[0]
+        exitdistance = re.findall(r'(?<=Exit:)\d+\.\d+',line)[0]
+        if len(re.findall(r'(?<=Flashlight:)\d+\.\d+',line)) == 1:
+            flashlight = re.findall(r'(?<=Flashlight:)\d+\.\d+',line)[0]
+        else:
+            flashlight = None
+        if len(re.findall(r'(?<=Door:)\d+\.\d+',line)) == 1:
+            sdoor = re.findall(r'(?<=Door:)\d+\.\d+',line)[0]
+        else:
+            sdoor = None
+        CI = ChaseInformation(float(speed),float(aspeed),float(apos),float(hlength),float(exitdistance),flashlight,sdoor)
+    return CI
+
+print(getInput().sdoor)
